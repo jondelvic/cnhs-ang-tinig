84d01c02ff3bbd7e5246882bae35a45ce1f42189
Jon Delvic Calub <95561006+jondelvic@users.noreply.github.com>
2023-05-22 18:16:49 +0800

    Toggle button fix

--- js/nav.js
+++ js/nav.js
@@ -4,16 +4,14 @@ const navToggle = document.querySelector(".mobile-nav-toggle");
 navToggle.addEventListener('click', () => {
     const visibility = primaryNav.getAttribute('data-visible');
 
-    // console.log(visibility);
-
     if (visibility === "false") {
-        primaryNav.setAttribute('data-visible', true);
-        navToggle.setAttribute('aria-expanded', true);
+        primaryNav.setAttribute("data-visible", true);
+        navToggle.setAttribute("aria-expanded", true);
     } else if (visibility === "true") {
-        primaryNav.setAttribute('data-visible', false);
-        navToggle.setAttribute('aria-expanded', false);
+        primaryNav.setAttribute("data-visible", false);
+        navToggle.setAttribute("aria-expanded", false);
     }
-})
+});
 
 let resizeTimer;
 window.addEventListener("resize", () => {
